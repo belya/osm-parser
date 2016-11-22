bf6a9048ec9397573d6b58ab4295e77a4a8468dd
Blackmoon <belchikov1996@gmail.com>
2016-11-22 18:42:44 +0300

    Fixed app

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ var ejs = require('ejs');
 var fileSave = require('file-save');
 var utils = require('./utils')
 
-var query = "";
+// var query = '[out:json];(node["place"];); out body; >; out skel qt;'
+var query = process.argv[2];
 osmquery.query_overpass(query, function(error, data) {
   var geojson = osmtogeojson(data);
+  utils.identify(geojson)
   ejs.renderFile(utils.template(geojson), {geojson: geojson}, {}, function(error, string) {
     fileSave(utils.name(geojson)).write(string, 'utf8')
   });
--- utils.js
+++ utils.js
@@ -1,10 +1,14 @@
 var translit = require('translit-russian');
 var isSubset = require('is-subset');
 var templates = {
-  '/templates/': {}
+  'city': {}
 }
 
 module.exports = {
+  identify: function(geojson) {
+    geojson.identifier = translit(geojson.name)
+  },
+
   template: function(geojson) {
     for(var templateName in templates) {
       var condition = templates[templateName];
@@ -13,7 +17,7 @@ module.exports = {
     }
   },
 
-  name: function(name) {
-    return './results/' + translit(name) + '.scs';
+  name: function(geojson) {
+    return './results/' + geojson.identifier + '.scs';
   }
 }
